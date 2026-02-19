8919385dc63593464609d5b2446bf639193f4dbc
lihlerkhumalo <langelihle075@gmail.com>
2026-02-19 11:43:23 +0200

    added axios and fetched api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInputElement = document.querySelector(".search-form-input");
-    let city = document.querySelector("#city")
-    city.innerHTML = searchInputElement.value
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value;
 
+  
+  searchCity(searchInputElement.value);
 }
 
+function updateWeather(response) {
+    console.log(response.data)
+  let temperatureElement = document.querySelector("#temperatureInfo");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current) + "°C";
+}
+
+function searchCity(city) {
+  let apiKey = "de006o7c70fb22fbbf301edt429533ae";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let url =`let url = "https://jsonplaceholder.typicode.com/users/1`
+  axios.get(apiUrl).then(updateWeather);
+}
 
 let searchElement = document.querySelector("#search-form");
-searchElement.addEventListener("submit",handleSearchSubmit);
+searchElement.addEventListener("submit", handleSearchSubmit);
